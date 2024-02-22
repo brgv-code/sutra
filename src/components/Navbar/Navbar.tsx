5a6b3f570a85787f1735eabb3554e474e5178c47
brgv-code <bhargav.virupakshi@gmail.com>
2024-02-22 17:36:25 +0100

    intial layout

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,16 +2,13 @@
 import Links from './links/Links'
 
 function Navbar() {
-    return (
-        <div>
-            <div>Logo</div>
-            <div>
-                <Links />
-
-            </div>
-        </div>
-
-    )
+	return (
+		<div className='flex  justify-between pt-5 px-24'>
+			<div className=''>Logo </div>
+			<div className=''>
+				<Links />
+			</div>
+		</div>
+	)
 }
-
 export default Navbar
