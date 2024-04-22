5b25de11a95adaaa1fe478760c9a4b75cbe102dc
brgv-code <bhargav.virupakshi@gmail.com>
2024-04-22 22:39:58 +0200

    reusable component rotating text contact

--- src/components/ui/rotating-text.tsx
+++ src/components/ui/rotating-text.tsx
@@ -1,5 +1,25 @@
 import Image from 'next/image'
-const RotatingText = () => {
+
+export interface RotatingTextProps {
+	text: string
+	repeat: number
+	divider: string
+	direction: 'clockwise' | 'counterclockwise'
+}
+
+const RotatingText: React.FC<RotatingTextProps> = ({
+	text,
+	repeat,
+	divider,
+	direction,
+}) => {
+	const splitText = text.split('')
+	const repeatedText = Array(repeat)
+		.fill([...splitText, divider])
+		.flat()
+
+	const directionValue = direction === 'clockwise' ? 1 : -1
+	if (directionValue === -1) repeatedText.reverse()
 	return (
 		<div className='relative'>
 			<Image
@@ -11,32 +31,16 @@ const RotatingText = () => {
 				className='rounded-full w-[6.2rem] h-[6.2rem]  fixed bg-black overflow-clip	bottom-10 border border-white/[0.5]
                 right-10'
 			/>
-			<div className='circle '>
-				<span>C</span>
-				<span>o</span>
-				<span>n</span>
-				<span>t</span>
-				<span>a</span>
-				<span>c</span>
-				<span>t</span>
-				<span>.</span>
-				<span>C</span>
-				<span>o</span>
-				<span>n</span>
-				<span>t</span>
-				<span>a</span>
-				<span>c</span>
-				<span>t</span>
-				<span>.</span>
-				<span>C</span>
-				<span>o</span>
-				<span>n</span>
-				<span>t</span>
-				<span>a</span>
-				<span>c</span>
-				<span>t</span>
+			<div
+				className='circle '
+				style={{ '--direction': directionValue } as React.CSSProperties}
+			>
+				{repeatedText.map((char, index) => (
+					<span key={index}>{char}</span>
+				))}
 			</div>
 		</div>
 	)
 }
+
 export default RotatingText
