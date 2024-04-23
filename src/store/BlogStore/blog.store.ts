21f51cb8b55fdbfc97a3a2a5810016a62b519370
brgv-code <bhargav.virupakshi@gmail.com>
2024-04-23 10:13:15 +0200

    store not needed. fix errors in prod build

--- src/store/BlogStore/blog.store.ts
+++ src/store/BlogStore/blog.store.ts
@@ -1,30 +1,30 @@
-import { BlogCardProps } from '@/components/ui/blog-card'
-import { MultiSelectTags } from '@/lib/notion'
-import { create } from 'zustand'
+// import { BlogCardProps } from '@/components/ui/blog-card'
+// import { MultiSelectTags } from '@/lib/notion'
+// import { create } from 'zustand'
 
-type BlogActions = {
-	setTitle: (title: BlogCardProps['title']) => void
-	setDate: (date: BlogCardProps['date']) => void
-	setContent: (content: BlogCardProps['content']) => void
-	setCover: (cover: BlogCardProps['cover']) => void
-	setTags: (tags: BlogCardProps['tags']) => void
-	setReadingTime: (reading_time: BlogCardProps['reading_time']) => void
-}
+// type BlogActions = {
+// 	setTitle: (title: BlogCardProps['title']) => void
+// 	setDate: (date: BlogCardProps['date']) => void
+// 	setContent: (content: BlogCardProps['content']) => void
+// 	setCover: (cover: BlogCardProps['cover']) => void
+// 	setTags: (tags: BlogCardProps['tags']) => void
+// 	setReadingTime: (reading_time: BlogCardProps['reading_time']) => void
+// }
 
-export const useBlogStore = create<BlogCardProps & BlogActions>(set => ({
-	title: '',
-	date: '',
-	content: '',
-	cover: '',
-	tags: [],
-	id: '',
-	reading_time: '',
-	setTitle: (title: BlogCardProps['title']) =>
-		set(state => ({ ...state, title })),
-	setDate: (date: string) => set(state => ({ ...state, date })),
-	setContent: (content: string) => set(state => ({ ...state, content })),
-	setCover: (cover: string) => set(state => ({ ...state, cover })),
-	setTags: (tags: MultiSelectTags[]) => set(state => ({ ...state, tags })),
-	setReadingTime: (reading_time: string) =>
-		set(state => ({ ...state, reading_time })),
-}))
+// export const useBlogStore = create<BlogCardProps & BlogActions>(set => ({
+// 	title: '',
+// 	date: '',
+// 	content: '',
+// 	cover: '',
+// 	tags: [],
+// 	id: '',
+// 	reading_time: '',
+// 	setTitle: (title: BlogCardProps['title']) =>
+// 		set(state => ({ ...state, title })),
+// 	setDate: (date: string) => set(state => ({ ...state, date })),
+// 	setContent: (content: string) => set(state => ({ ...state, content })),
+// 	setCover: (cover: string) => set(state => ({ ...state, cover })),
+// 	setTags: (tags: MultiSelectTags[]) => set(state => ({ ...state, tags })),
+// 	setReadingTime: (reading_time: string) =>
+// 		set(state => ({ ...state, reading_time })),
+// }))
