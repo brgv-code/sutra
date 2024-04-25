aef251e684a53e4389e599344648cbc4a9fc3675
brgv-code <bhargav.virupakshi@gmail.com>
2024-04-25 21:41:17 +0200

    favicon, toast notification setup

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,8 +1,22 @@
+'use client'
+import ToastNotification from '@/components/ui/toast-notification'
+import { tailwindColors } from '@/lib/tailwindColors'
 import Link from 'next/link'
+import toast from 'react-hot-toast'
 
 const Notfound = () => {
 	return (
-		<div>
+		<div
+			onClick={() =>
+				toast.custom(
+					<ToastNotification
+						message={'oh No!'}
+						icon={'/brgv_logo.png'}
+						color={tailwindColors.toast.error}
+					/>,
+				)
+			}
+		>
 			<h4>Notfound</h4>
 			<Link href='/'>Return Home</Link>
 		</div>
