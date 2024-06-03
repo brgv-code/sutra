25612cc31d9d2eec31c5f33a572d91c9b93e5438
brgv-code <bhargav.virupakshi@gmail.com>
2024-06-03 22:30:00 +0200

    ui changes

---
+++ src/app/template.tsx
@@ -0,0 +1,32 @@
+// components/Layout.js
+'use client'
+import { AnimatePresence, motion } from 'framer-motion'
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
+const variants = {
+	hidden: { opacity: 0, filter: 'blur(10px)' },
+	glow: { filter: 'brightness(1.5)', transition: { duration: 0.75 } },
+	fadeOut: { opacity: 0, transition: { duration: 0.5 } },
+	enter: {
+		opacity: 1,
+		filter: 'brightness(1)',
+		transition: { duration: 0.75 },
+	},
+}
+const Template = ({ children }: { children: React.ReactNode }) => {
+	return (
+		<AnimatePresence mode='wait'>
+			<motion.div
+				initial='hidden'
+				animate='enter'
+				variants={variants}
+				exit='glow'
+				className='flex justify-center w-full motion-div'
+			>
+				{children}
+			</motion.div>
+		</AnimatePresence>
+	)
+}
+
+export default Template
