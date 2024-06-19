42640657d241edbeef36fd3fa4eb514f42e3398b
brgv-code <bhargav.virupakshi@gmail.com>
2024-06-19 21:04:40 +0200

    remove db & strapi

--- cms/src/api/sutra/controllers/sutra.ts
+++
@@ -1,7 +0,0 @@
-/**
- * sutra controller
- */
-
-import { factories } from '@strapi/strapi'
-
-export default factories.createCoreController('api::sutra.sutra');
--- cms/src/api/sutra/routes/sutra.ts
+++
@@ -1,7 +0,0 @@
-/**
- * sutra router
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreRouter('api::sutra.sutra');
