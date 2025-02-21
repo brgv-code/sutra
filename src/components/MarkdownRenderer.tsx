740f385bbe288245e5b22ad38bb4cf41f44616c9
Bhargav <bhargav@foundamental.vc>
2025-02-21 18:24:08 +0100

    fix: simplify mdx rendering, fix blog content render

--- src/components/MarkdownRenderer.tsx
+++ src/components/MarkdownRenderer.tsx
@@ -6,7 +6,9 @@ import CustomLink from '@/components/ui/custom-links'
 import CustomListItem from '@/components/ui/custom-list'
 import CodeBlock from './ui/custom-code-block'
 import CustomBold from './ui/custom-bold'
-
+import { oneDark } from 'react-syntax-highlighter/dist/esm/styles/prism'
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
+import CustomCode from './ui/custom-code-block'
 interface CodeProps {
 	children?: React.ReactNode
 	className?: string
@@ -27,41 +29,45 @@ const MarkdownRenderer: React.FC<MarkdownRendererProps> = ({
 	content,
 	className = '',
 }) => {
-	const components: Components = {
-		code: ({ children, className, ...props }) => {
+	const components = {
+		h1: (props: any) => <CustomBold {...props} />,
+		h2: (props: any) => <CustomBold  fontSize='2xl' underline={false} {...props} />,
+		h3: (props: any) => <CustomBold  fontSize='xl'  {...props} />,
+		a: (props: any) => <CustomLink {...props} />,
+		li: (props: any) => <CustomListItem {...props} />,
+		code({ node, inline, className, children, ...props }: any) {
 			const match = /language-(\w+)/.exec(className || '')
-			const isInline = !match && !className
-
-			if (isInline) {
-				return (
-					<InlineCode className={className} {...props}>
-						{children}
-					</InlineCode>
-				)
-			}
-
-			return (
-				<CodeBlock className={className} {...props}>
-					{children}
-				</CodeBlock>
-			)
-		},
-		a: ({ node, children, href }) => (
-			<CustomLink href={href}>{children}</CustomLink>
-		),
-		li: CustomListItem,
-		h1: CustomBold,
-		h2: ({ children, ...props }) => {
-			return (
-				<CustomBold
-					fontSize={'xl'}
-					style={{ marginTop: '1rem', marginBottom: '1rem' }}
+			return !inline && match ? (
+				
+				<CustomCode
+				className='m-auto '
+				><SyntaxHighlighter
+					style={{
+						...oneDark,
+						'pre[class*="language-"]': {
+							...oneDark['pre[class*="language-"]'],
+							background: 'transparent',
+							fontSize: '1rem',
+						},
+						'code[class*="language-"]': {
+							...oneDark['code[class*="language-"]'],
+							background: 'transparent',
+							fontSize: '1rem',
+						}
+					}}
+					language={match[1]}
+					PreTag="div"
 					{...props}
 				>
+					{String(children).replace(/\n$/, '')}
+				</SyntaxHighlighter>
+				</CustomCode>
+			) : (
+				<InlineCode className={className} {...props}>
 					{children}
-				</CustomBold>
+				</InlineCode>
 			)
-		},
+		}
 	}
 
 	return (
