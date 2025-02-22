9b331c2c27255d0c396dc84929edb4f1cad610e7
brgv-code <bhargav.virupakshi@gmail.com>
2025-02-22 21:56:18 +0100

    fix: new blog posts

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -2,6 +2,7 @@ import { getFileBySlug, getFiles } from '@/lib/mdx'
 import 'highlight.js/styles/github-dark.css'
 import { Space_Mono } from 'next/font/google'
 import MarkdownRenderer from '@/components/MarkdownRenderer'
+import { notFound } from 'next/navigation'
 
 const mono = Space_Mono({
 	weight: ['400', '700'],
@@ -22,6 +23,7 @@ export default async function BlogPost({
 	params: { slug: string }
 }) {
 	const post = await getFileBySlug('blog', params.slug)
+	if (!post) return notFound()
 	return (
 		<div className={` text-white min-h-screen ${mono.variable}`}>
 			<article className='max-w-[800px] mx-auto px-4 py-20'>
@@ -36,8 +38,8 @@ export default async function BlogPost({
 				<MarkdownRenderer
 					className='border border-white/10 rounded-xl p-6 shadow-xl'
 					content={post.contentHtml}
-                />
-			</article> 
+				/>
+			</article>
 		</div>
 	)
 }
